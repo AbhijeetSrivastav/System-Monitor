1582ec4f458c3d118b81390400d996a61fefcc5b
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-02-21 17:01:53 +0530

    exception handler added

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 "APP Runner"
 
+import sys
+
 from monitor.ui.splash import SplashScreen
-from monitor.utilities.utils import DPIAwareness
+from monitor.utils import DPIAwareness
+from monitor.logger import logging
+from monitor.exception import MonitorException
+
 
 if __name__ == "__main__":
-    system_monitor = SplashScreen()
-    DPIAwareness(system_monitor)
-    system_monitor.mainloop()
+    try:
+        logging.info(f"App started")
+        system_monitor = SplashScreen()
+        DPIAwareness(system_monitor)
+        system_monitor.mainloop()
+        logging.info(f"App closed")
+    except Exception as e:
+        raise MonitorException(e, sys)

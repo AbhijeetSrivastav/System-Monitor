85d2f931a35887fe565ba38c45216bdbdaeb8f2a
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-02-21 13:50:45 +0530

    DPI Awareenss added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 "APP Runner"
 
 from monitor.ui.splash import SplashScreen
+from monitor.utilities.utils import DPIAwareness
 
 if __name__ == "__main__":
     system_monitor = SplashScreen()
+    DPIAwareness(system_monitor)
     system_monitor.mainloop()
